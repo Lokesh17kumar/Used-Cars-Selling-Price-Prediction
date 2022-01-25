207a56a81fce98bc53a2da64d3172f81cb730692
Y Lokesh Kumar <lokesh15kumar1999@gmail.com>
2022-01-25 16:42:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@ mkdir -p ~/.streamlit/
 
 echo "
 [theme]
-primaryColor = 'F36295'
-backgroundColor = '#F0FF33'
-secondaryBackgroundColor = '#3183D1'
-textColor = '#03080C'
-font = 'sans-serif'
+primaryColor = '#41B853'
+backgroundColor = '#161B40'
+secondaryBackgroundColor = '#EFA500'
+textColor = '#FFFFFF'
+font = 'serif'
 [server]
 port = $PORT
 enableCORS = false
